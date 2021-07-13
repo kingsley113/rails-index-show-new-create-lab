70a12a568df110ce7f044ca674718b7a17c93e6a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-07-12 17:00:10 -0700

    commit tree 4c07771a540683601e1abdee6d53deb7682ca36d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: 'freestuff', store: 'MegaMart')
+Coupon.create(coupon_code: 'cheepstuff', store: 'Local Expensive Family Store')
+Coupon.create(coupon_code: 'freeshitaki', store: 'Mushroom Outlet')
+Coupon.create(coupon_code: 'guddeel4u', store: 'Smart Shopper')
